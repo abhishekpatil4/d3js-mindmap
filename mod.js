db95e786688dccd25f822cb7e3809f7f30a99b88
Abhishek Patil <abishkpatil@gmail.com>
2023-07-03 16:27:00 +0530

    scritp extract name and url from json data

---
+++ mod.js
@@ -0,0 +1,215 @@
+const jsonData = [
+    {
+        "name": ".DS_Store",
+        "path": ".DS_Store",
+        "sha": "99728ffbd4fc130f86a2c016a486f49203c6ef26",
+        "size": 8196,
+        "url": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/.DS_Store?ref=main",
+        "html_url": "https://github.com/abhishekpatil4/gitglance/blob/main/.DS_Store",
+        "git_url": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/99728ffbd4fc130f86a2c016a486f49203c6ef26",
+        "download_url": "https://raw.githubusercontent.com/abhishekpatil4/gitglance/main/.DS_Store",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/.DS_Store?ref=main",
+            "git": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/99728ffbd4fc130f86a2c016a486f49203c6ef26",
+            "html": "https://github.com/abhishekpatil4/gitglance/blob/main/.DS_Store"
+        }
+    },
+    {
+        "name": ".next",
+        "path": ".next",
+        "sha": "5040e88fc518295b280efb94cd9ec645048cea20",
+        "size": 0,
+        "url": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/.next?ref=main",
+        "html_url": "https://github.com/abhishekpatil4/gitglance/tree/main/.next",
+        "git_url": "https://api.github.com/repos/abhishekpatil4/gitglance/git/trees/5040e88fc518295b280efb94cd9ec645048cea20",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/.next?ref=main",
+            "git": "https://api.github.com/repos/abhishekpatil4/gitglance/git/trees/5040e88fc518295b280efb94cd9ec645048cea20",
+            "html": "https://github.com/abhishekpatil4/gitglance/tree/main/.next"
+        }
+    },
+    {
+        "name": "README.md",
+        "path": "README.md",
+        "sha": "21acb5df248d265a11d5a0c0dc747cf64885c22c",
+        "size": 659,
+        "url": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/README.md?ref=main",
+        "html_url": "https://github.com/abhishekpatil4/gitglance/blob/main/README.md",
+        "git_url": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/21acb5df248d265a11d5a0c0dc747cf64885c22c",
+        "download_url": "https://raw.githubusercontent.com/abhishekpatil4/gitglance/main/README.md",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/README.md?ref=main",
+            "git": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/21acb5df248d265a11d5a0c0dc747cf64885c22c",
+            "html": "https://github.com/abhishekpatil4/gitglance/blob/main/README.md"
+        }
+    },
+    {
+        "name": "app",
+        "path": "app",
+        "sha": "abf1e693e773f712cecd38e2f0a864b31eea0b1a",
+        "size": 0,
+        "url": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/app?ref=main",
+        "html_url": "https://github.com/abhishekpatil4/gitglance/tree/main/app",
+        "git_url": "https://api.github.com/repos/abhishekpatil4/gitglance/git/trees/abf1e693e773f712cecd38e2f0a864b31eea0b1a",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/app?ref=main",
+            "git": "https://api.github.com/repos/abhishekpatil4/gitglance/git/trees/abf1e693e773f712cecd38e2f0a864b31eea0b1a",
+            "html": "https://github.com/abhishekpatil4/gitglance/tree/main/app"
+        }
+    },
+    {
+        "name": "jsconfig.json",
+        "path": "jsconfig.json",
+        "sha": "2a2e4b3bf8ba1c86d96fc2f5786597ad77a0e5e9",
+        "size": 73,
+        "url": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/jsconfig.json?ref=main",
+        "html_url": "https://github.com/abhishekpatil4/gitglance/blob/main/jsconfig.json",
+        "git_url": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/2a2e4b3bf8ba1c86d96fc2f5786597ad77a0e5e9",
+        "download_url": "https://raw.githubusercontent.com/abhishekpatil4/gitglance/main/jsconfig.json",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/jsconfig.json?ref=main",
+            "git": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/2a2e4b3bf8ba1c86d96fc2f5786597ad77a0e5e9",
+            "html": "https://github.com/abhishekpatil4/gitglance/blob/main/jsconfig.json"
+        }
+    },
+    {
+        "name": "logo.png",
+        "path": "logo.png",
+        "sha": "530dd5c566aeda3580f3f6d26fbcafc545f7a1e2",
+        "size": 332838,
+        "url": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/logo.png?ref=main",
+        "html_url": "https://github.com/abhishekpatil4/gitglance/blob/main/logo.png",
+        "git_url": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/530dd5c566aeda3580f3f6d26fbcafc545f7a1e2",
+        "download_url": "https://raw.githubusercontent.com/abhishekpatil4/gitglance/main/logo.png",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/logo.png?ref=main",
+            "git": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/530dd5c566aeda3580f3f6d26fbcafc545f7a1e2",
+            "html": "https://github.com/abhishekpatil4/gitglance/blob/main/logo.png"
+        }
+    },
+    {
+        "name": "next.config.js",
+        "path": "next.config.js",
+        "sha": "767719fc4fba59345ae29e29159c9aff270f5819",
+        "size": 92,
+        "url": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/next.config.js?ref=main",
+        "html_url": "https://github.com/abhishekpatil4/gitglance/blob/main/next.config.js",
+        "git_url": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/767719fc4fba59345ae29e29159c9aff270f5819",
+        "download_url": "https://raw.githubusercontent.com/abhishekpatil4/gitglance/main/next.config.js",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/next.config.js?ref=main",
+            "git": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/767719fc4fba59345ae29e29159c9aff270f5819",
+            "html": "https://github.com/abhishekpatil4/gitglance/blob/main/next.config.js"
+        }
+    },
+    {
+        "name": "node_modules",
+        "path": "node_modules",
+        "sha": "0bda16aa3d130ba13231b74dd1f1962635d9662c",
+        "size": 0,
+        "url": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/node_modules?ref=main",
+        "html_url": "https://github.com/abhishekpatil4/gitglance/tree/main/node_modules",
+        "git_url": "https://api.github.com/repos/abhishekpatil4/gitglance/git/trees/0bda16aa3d130ba13231b74dd1f1962635d9662c",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/node_modules?ref=main",
+            "git": "https://api.github.com/repos/abhishekpatil4/gitglance/git/trees/0bda16aa3d130ba13231b74dd1f1962635d9662c",
+            "html": "https://github.com/abhishekpatil4/gitglance/tree/main/node_modules"
+        }
+    },
+    {
+        "name": "package-lock.json",
+        "path": "package-lock.json",
+        "sha": "ef3d95c44360acc9e700be9592863ca36cd262c3",
+        "size": 99784,
+        "url": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/package-lock.json?ref=main",
+        "html_url": "https://github.com/abhishekpatil4/gitglance/blob/main/package-lock.json",
+        "git_url": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/ef3d95c44360acc9e700be9592863ca36cd262c3",
+        "download_url": "https://raw.githubusercontent.com/abhishekpatil4/gitglance/main/package-lock.json",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/package-lock.json?ref=main",
+            "git": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/ef3d95c44360acc9e700be9592863ca36cd262c3",
+            "html": "https://github.com/abhishekpatil4/gitglance/blob/main/package-lock.json"
+        }
+    },
+    {
+        "name": "package.json",
+        "path": "package.json",
+        "sha": "e668874592e80ba82e65539b5b4741bd81337612",
+        "size": 426,
+        "url": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/package.json?ref=main",
+        "html_url": "https://github.com/abhishekpatil4/gitglance/blob/main/package.json",
+        "git_url": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/e668874592e80ba82e65539b5b4741bd81337612",
+        "download_url": "https://raw.githubusercontent.com/abhishekpatil4/gitglance/main/package.json",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/package.json?ref=main",
+            "git": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/e668874592e80ba82e65539b5b4741bd81337612",
+            "html": "https://github.com/abhishekpatil4/gitglance/blob/main/package.json"
+        }
+    },
+    {
+        "name": "postcss.config.js",
+        "path": "postcss.config.js",
+        "sha": "33ad091d26d8a9dc95ebdf616e217d985ec215b8",
+        "size": 82,
+        "url": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/postcss.config.js?ref=main",
+        "html_url": "https://github.com/abhishekpatil4/gitglance/blob/main/postcss.config.js",
+        "git_url": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/33ad091d26d8a9dc95ebdf616e217d985ec215b8",
+        "download_url": "https://raw.githubusercontent.com/abhishekpatil4/gitglance/main/postcss.config.js",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/postcss.config.js?ref=main",
+            "git": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/33ad091d26d8a9dc95ebdf616e217d985ec215b8",
+            "html": "https://github.com/abhishekpatil4/gitglance/blob/main/postcss.config.js"
+        }
+    },
+    {
+        "name": "public",
+        "path": "public",
+        "sha": "5efe8d1adeb9757d5b3d194aad655238775d3d7d",
+        "size": 0,
+        "url": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/public?ref=main",
+        "html_url": "https://github.com/abhishekpatil4/gitglance/tree/main/public",
+        "git_url": "https://api.github.com/repos/abhishekpatil4/gitglance/git/trees/5efe8d1adeb9757d5b3d194aad655238775d3d7d",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/public?ref=main",
+            "git": "https://api.github.com/repos/abhishekpatil4/gitglance/git/trees/5efe8d1adeb9757d5b3d194aad655238775d3d7d",
+            "html": "https://github.com/abhishekpatil4/gitglance/tree/main/public"
+        }
+    },
+    {
+        "name": "tailwind.config.js",
+        "path": "tailwind.config.js",
+        "sha": "a59b005251061d849a4cc91ce460b888a25da0fd",
+        "size": 539,
+        "url": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/tailwind.config.js?ref=main",
+        "html_url": "https://github.com/abhishekpatil4/gitglance/blob/main/tailwind.config.js",
+        "git_url": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/a59b005251061d849a4cc91ce460b888a25da0fd",
+        "download_url": "https://raw.githubusercontent.com/abhishekpatil4/gitglance/main/tailwind.config.js",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/abhishekpatil4/gitglance/contents/tailwind.config.js?ref=main",
+            "git": "https://api.github.com/repos/abhishekpatil4/gitglance/git/blobs/a59b005251061d849a4cc91ce460b888a25da0fd",
+            "html": "https://github.com/abhishekpatil4/gitglance/blob/main/tailwind.config.js"
+        }
+    }
+]
+  
+  const modifiedData = jsonData.map(({ name, url }) => ({ name, url }));
+  
+  console.log(modifiedData);
+  
