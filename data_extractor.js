32bc16c6c8b433adb764a67daf948fa19bea39b6
Abhishek Patil <abishkpatil@gmail.com>
2023-07-03 19:30:11 +0530

    data extractor

--- data_extractor.js
+++ data_extractor.js
@@ -1,11 +1,19 @@
-const fetch = require('node-fetch');
+async function fetchData() {
+    try {
+      const fetch = await import('node-fetch');
+      const response = await fetch.default('https://api.github.com/repos/abhishekpatil4/gitglance/contents');
+      let data = await response.json();
+      data = data.map(({ name, url }) => ({ name, url }));
+      console.log(data);
+      // You can access and use the fetched data here
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  }
+  
+  
 
-fetch('')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    // You can access and use the fetched data here
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+  function extractData(){
+    fetchData();  
+  }
+  
