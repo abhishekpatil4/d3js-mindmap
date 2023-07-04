440dda749d350cf18e6fe2ccc5908549771a9621
Abhishek Patil <abishkpatil@gmail.com>
2023-07-04 09:13:37 +0530

    visualised repo data

--- data_extractor.js
+++ data_extractor.js
@@ -1,32 +1,42 @@
+const fs = require('fs');
+
 async function fetchData(url) {
-    try {
-      const fetch = await import('node-fetch');
-      const response = await fetch.default(url);
-      let data = await response.json();
-      data = data.map(({ name, url }) => ({ name, url }));
-      return data;
-      // You can access and use the fetched data here
-    } catch (error) {
-      console.log('Error:', error);
+  try {
+    const fetch = await import('node-fetch');
+    const response = await fetch.default(url);
+    let data = await response.json();
+    data = data.map(({ name, url }) => ({ name, url }));
+    return data;
+    // You can access and use the fetched data here
+  } catch (error) {
+    console.log('Error:', error);
+  }
+}
+
+
+
+async function extractData(url) {
+  let data = await fetchData(url);
+  for (const item of data) {
+    const isFile = item.url.lastIndexOf('.') > item.url.lastIndexOf('/');
+    if (isFile) {
+      console.log(`${item.name} is a file.`);
+    } else {
+      console.log(`${item.name} is a directory.`);
+      let temp = await fetchData(item.url);
+      item.children = temp;
+      // console.log(temp);
     }
   }
-  
-  
+  // console.log(data);
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+    if (err) {
+      console.error('An error occurred while writing the file:', err);
+      return;
+    }
+    console.log('JSON file has been created successfully.');
+  });
 
-  async function extractData(){
-    let data = await fetchData('https://api.github.com/repos/abhishekpatil4/gitglance/contents');  //fetching actual repo data
-    for (const item of data) {
-        const isFile = item.url.lastIndexOf('.') > item.url.lastIndexOf('/');
-        
-        if (isFile) {
-          console.log(`${item.name} is a file.`);
-        } else {
-          console.log(`${item.name} is a directory.`);
-          let temp = await fetchData(item.url);
-          console.log(temp);
-        }
-      }
-  }
+}
 
-  extractData();
-  
+extractData('https://api.github.com/repos/abhishekpatil4/gitglance/contents'); //fetching actual repo data
